69d2ccb0ddbd4c974f94f7eb6f557399111731fb
Marcos Dimitrio <marcos@dimitrio.com>
2020-09-08 13:41:06 -0300

    Install Costura.Fody for compatibility with MahApps.Metro

--- SolutionCreator.Wpf/App.xaml.cs
+++ SolutionCreator.Wpf/App.xaml.cs
@@ -14,10 +14,6 @@ namespace SolutionCreator.Wpf
 
         private void Application_Startup(object sender, StartupEventArgs e)
         {
-            // Merging dlls into a single .exe with wpf
-            // https://stackoverflow.com/questions/1025843/merging-dlls-into-a-single-exe-with-wpf#4995039
-            AppDomain.CurrentDomain.AssemblyResolve += OnResolveAssembly;
-
             var view = new MainWindow();
             Current.MainWindow = view;
             Current.MainWindow.Show();
@@ -34,24 +30,6 @@ namespace SolutionCreator.Wpf
         {
             var errorMessage = string.Format("ERROR:\r\n\r\n{0}", string.Join("\r\n", ((Exception)e.ExceptionObject).GetAllMessages()));
             MessageBox.Show(errorMessage, "Unexpected error", MessageBoxButton.OK, MessageBoxImage.Error);
-        }
-
-        private static Assembly OnResolveAssembly(object sender, ResolveEventArgs args)
-        {
-            var executingAssembly = Assembly.GetExecutingAssembly();
-            var assemblyName = new AssemblyName(args.Name);
-
-            var path = $"{assemblyName.Name}.dll";
-            if (assemblyName.CultureInfo.Equals(CultureInfo.InvariantCulture) == false) path = string.Format(@"{0}\{1}", assemblyName.CultureInfo, path);
-
-            using (var stream = executingAssembly.GetManifestResourceStream(path))
-            {
-                if (stream == null) return null;
-
-                var assemblyRawBytes = new byte[stream.Length];
-                stream.Read(assemblyRawBytes, 0, assemblyRawBytes.Length);
-                return Assembly.Load(assemblyRawBytes);
-            }
         }
 
     }
